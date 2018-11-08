768eefbe47037740ac7c9dd3dd5e96710ef61988
aimashi620 <1301913120@qq.com>
2018-11-08 16:27:07 +0800

    1.修改jdbc密码；2.修改自动化部署脚本的参数

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ killTomcat()
     	kill -9 $pid
     fi
 }
-cd $PROJ_PATH/order
+cd $PROJ_PATH/weikai_server
 mvn clean install
 
 #停tomcat
@@ -28,13 +28,13 @@ killTomcat
 #删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -f $TOMCAT_APP_PATH/webapps/weikai_server.war
 
 #复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/weikai_server/target/weikai_server.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+mv weikai_server.war ROOT.war
 
 #启动Tomcat
 cd $TOMCAT_APP_PATH/
